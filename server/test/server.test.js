b9c2319d6a74656ce15e4ba62628cf482dd8dc41
emeery <gerardoemery@live.com.mx>
2018-11-16 05:20:33 -0600

    get todo

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,13 @@ const expect = require('expect');
 const {app} = require('./../server');
 const {Todo} = require('./../model/todo');
 // elimina todos los items del todo antes del test
+const todos = [{tarea: 'primer test todo'}, {tarea: 'segundo test todo'}];
 beforeEach(done => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
-describe('post /', () => {
+describe('post /todos', () => {
     it('deberia crear un todo', (done) => {
         var tarea = 'test todo';
         request(app)
@@ -19,7 +22,7 @@ describe('post /', () => {
         })
         .end((err, res)=> {
             if(err) { return done(err); }
-            Todo.find().then(todos => {
+            Todo.find({tarea}).then(todos => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].tarea).toBe(tarea);
                 done();
@@ -34,9 +37,21 @@ describe('post /', () => {
         .end((err, res)=> {
             if(err) { return done(err); }
             Todo.find().then(todos => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch(e => done(e));
         });
+    });
+});
+
+describe('get /todos', () => {
+    it('deberia obtener todos', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res)=> {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
     });
 });
